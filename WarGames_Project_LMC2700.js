b46803519b0fc522ef2b2e9e23db25dcd785a9f9
Eugene Li <eugene.gao.li@gmail.com>
2026-03-23 22:48:20 -0400

    initial

---
+++ WarGames_Project_LMC2700.js
@@ -0,0 +1,13 @@
+function preload() {
+  preLoadFont();
+}
+function setup() {
+  createCanvas(800, 500);
+  loadWarGamesFont();
+}
+function draw() {
+  background(0);
+  stroke(255);
+  
+  hpLineFont.drawString("2erwewt", 80,420); 
+}
